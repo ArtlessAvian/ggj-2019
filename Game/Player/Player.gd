637f70dfe6c3966e18db2e6501bfcaacaade7236
AnthoaLe <anhoal@uci.edu>
2019-01-25 21:54:23 -0800

    simple movement script walk left+right and gravity

--- Game/Player/Player.gd
+++ Game/Player/Player.gd
@@ -1,15 +1,25 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
+const move_speed = 200
+const gravity = 300
+
+var vel = Vector2()
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	if is_on_floor():
+		vel.y = 0
+	else:
+		vel.y += delta * gravity
+	
+	if Input.is_action_pressed("ui_left"):
+		vel.x = -move_speed
+	elif Input.is_action_pressed("ui_right"):
+		vel.x = move_speed
+	else:
+		vel.x = 0
+	
+	self.move_and_slide(vel, Vector2(0, -1))
